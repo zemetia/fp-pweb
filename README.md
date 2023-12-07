d3ace71b884179635a3627dfdfb1e07a6015d229
yoel-mc <yoel@mulaicoding.id>
2023-12-07 17:01:59 +0700

    feat: add tables | doc: update doc

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # fp-pweb
+
+## Database
+
+1. Users
+
+- id
+- name
+- balance
+- photo_profile
+- created_at
+
+2. Wallets
+
+- id (Token)
+- name
+- description
+- creator_id
+- balance
+- created_at
+- update_at
+
+3. UserOnWallets
+
+- id
+- wallet_id
+- user_id
+- created_at
+
+4. Mutasi
+
+- id
+- type (user/wallet)
+- uow_id (user or wallet ID)
+- log
+- amount
+- created_at
---
+++ table.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    balance FLOAT NOT NULL DEFAULT 0,
+    photo_profile VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wallets (
+    id VARCHAR(6) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    creator_id INT,
+    balance FLOAT NOT NULL DEFAULT 0,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES Users(id)
+);
+
+CREATE TABLE user_on_wallets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    wallet_id VARCHAR(6),
+    user_id INT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (wallet_id) REFERENCES Wallets(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Mutasi mutation(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(10) NOT NULL,
+    uow_id VARCHAR(6) NOT NULL,
+    log TEXT,
+    amount FLOAT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
