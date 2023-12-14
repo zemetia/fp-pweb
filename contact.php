e87a317c0d66f944bbbbb53c5b950fd74937e781
Angga <angga060803@gmail.com>
2023-12-14 15:09:38 +0700

    contact page

---
+++ contact.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Porto - Tailwind Template</title>
+    <link rel="stylesheet" href="assets/css/tailwind.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js"
+        integrity="sha512-7x3zila4t2qNycrtZ31HO0NnJr8kg2VI67YLoRSyi9hGhRN66FHYWr7Axa9Y1J9tGYHVBPqIjSE1ogHrJTz51g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body class="bg-gray-100 grid grid-cols-2">
+    <!-- Left Column -->
+    <div>
+    <section class="py-2.5 md:py-4">
+
+<div class="container max-w-screen-xl mx-auto px-4">
+
+    <nav class="flex items-center justify-between mb-20">
+            <div class="mr-4">
+                <span class="text-lg font-bold">PUPUAN</span>
+            </div>
+
+            <div class="mr-4">
+                <span class="text-lg font-bold">PUPUAN</span>
+            </div>
+    </nav>
+
+    <div class="text-center">
+        <div class="flex justify-center mb-10">
+            <img src="assets/image/yoel.jpeg" alt="Image" class="round-image">
+        </div>
+
+        <h6 class="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">5025211078</h6>
+
+        <h1 class="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">Yoel Montanus Sitorus</h1>
+
+        <p class="font-normal text-gray-600 text-md md:text-xl mb-16">yoel@gmail.com || +62 822-1398-9336</p>
+
+        <a href="https://saweria.co/yownggaaa" class="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500">Laptop Baru</a>
+    </div>
+
+</div>
+
+</section>
+
+<footer class="py-10 md:py-16 mb-20 md:mb-40 lg::mb-52">
+
+<div class="container max-w-screen-xl mx-auto px-4">
+
+    <div class="text-center">
+
+
+        <div class="flex items-center justify-center space-x-8">
+            <a href="https://github.com/zemetia" class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+                <i data-feather="at-sign" class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
+            </a>
+
+            <a href="https://www.instagram.com/yozymer/" class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+                <i data-feather="instagram" class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
+            </a>
+        </div>
+    </div>
+
+</div>
+
+</footer>
+
+
+<script>
+feather.replace()
+</script>
+    </div>
+
+    <!-- Right Column -->
+    <div>
+    <section class="py-2.5 md:py-4">
+
+<div class="container max-w-screen-xl mx-auto px-4">
+
+    <nav class="flex items-center justify-between mb-20">
+            <div class="mr-4">
+                <span class="text-lg font-bold">PUPUAN</span>
+            </div>
+
+            <div class="mr-4">
+                <span class="text-lg font-bold">PUPUAN</span>
+            </div>
+    </nav>
+
+    <div class="text-center">
+        <div class="flex justify-center mb-10">
+            <img src="assets/image/angga.jpeg" alt="Image" class="round-image">
+        </div>
+
+        <h6 class="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">5025211241</h6>
+
+        <h1 class="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">Anggara Saputra</h1>
+
+        <p class="font-normal text-gray-600 text-md md:text-xl mb-16">angga060803@gmail.com || +62 812-6017-9930</p>
+
+        <a href="https://saweria.co/yownggaaa" class="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500">Jajan Kopi</a>
+    </div>
+
+</div>
+
+</section>
+
+<footer class="py-10 md:py-16 mb-20 md:mb-40 lg::mb-52">
+
+<div class="container max-w-screen-xl mx-auto px-4">
+
+    <div class="text-center">
+
+        <div class="flex items-center justify-center space-x-8">
+            <a href="https://github.com/anggarasr666" class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+                <i data-feather="at-sign" class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
+            </a>
+
+            <a href="https://www.instagram.com/yownggaaa/" class="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+                <i data-feather="instagram" class="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
+            </a>
+        </div>
+    </div>
+
+</div>
+
+</footer>
+
+
+<script>
+feather.replace()
+</script>
+    </div>
+
+</body>
+</html>
